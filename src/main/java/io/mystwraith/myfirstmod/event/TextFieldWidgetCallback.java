b54f2535cd8e6310b26bd2f60ae24090fe4f8b98
Ink-Life <77246251+Ink-Life@users.noreply.github.com>
2023-06-17 13:28:15 -0700

    Initial commit

---
+++ src/main/java/io/mystwraith/myfirstmod/event/TextFieldWidgetCallback.java
@@ -0,0 +1,16 @@
+package io.mystwraith.myfirstmod.event;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
+public interface TextFieldWidgetCallback {
+    Event<TextFieldWidgetCallback> EVENT = EventFactory.createArrayBacked(TextFieldWidgetCallback.class, 
+        (listeners) -> (chr, modifiers) -> {
+            for (TextFieldWidgetCallback listener : listeners) {
+                listener.keyPressed(chr, modifiers);
+            }
+        }
+    );
+
+    void keyPressed(char chr, int modifiers);
+}
---
+++ src/main/java/io/mystwraith/myfirstmod/mixin/TextFieldMixin.java
@@ -0,0 +1,18 @@
+package io.mystwraith.myfirstmod.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import io.mystwraith.myfirstmod.event.TextFieldWidgetCallback;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+
+@Mixin(TextFieldWidget.class)
+public class TextFieldMixin {
+    
+    @Inject(method = "charTyped(CI)Z", at = @At(value = "INVOKE", target = "java/lang/Character.toString (C)Ljava/lang/String"))
+    public void onKeyPressed(char chr, int modifiers, CallbackInfoReturnable ci) {
+        TextFieldWidgetCallback.EVENT.invoker().keyPressed(chr, modifiers);
+    }
+}
